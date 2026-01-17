8fd86a50d99ef8e0e685e59a7939cc484ada0115
rheymarcibanezpog123 <rheymarc.ibanez@hcdc.edu.ph>
2026-01-17 15:19:40 +0800

    Aloysius Benedict P. Quiño

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -18,11 +18,11 @@ export default function HomeScreen() {
         />
       }>
       <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+        <ThemedText type="title">Aloysius Benedict P. Quiño</ThemedText>
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <ThemedText type="subtitle">Hello World</ThemedText>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
